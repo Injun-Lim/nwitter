8b3f70273257046c3bd7cf252c7091828b1f3db8
Injun-Lim <jz6865@naver.com>
2022-05-09 16:43:14 +0900

    기초 셋팅

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+export default firebase.initializeApp(firebaseConfig);
+
+/* .env
+REACT_APP_API_KEY ="AIzaSyDX0lYWnT9EvLLE8l1bhigrCMXGivzlzlg"
+REACT_APP_AUTH_DOMAIN ="nwitter-7247d.firebaseapp.com"
+REACT_APP_DATABASE_URL = ""
+REACT_APP_PROJECT_ID ="nwitter-7247d"
+REACT_APP_STORAGE_BUCKET ="nwitter-7247d.appspot.com"
+REACT_APP_MESSAGIN_ID ="444386208408"
+REACT_APP_APP_ID ="1:444386208408:web:7f18036e738f9ecbd0beed" */
